b8fb19cc37dbdbbbe39f4be185b68248e707f214
1U25CS001-AATHIRAJ <aathirajs29@gmail.com>
2026-08-21 12:46:25 +0530

    Modify SQL script to create student table

--- starter/answers.sql
+++ starter/answers.sql
@@ -1,8 +1,8 @@
-create database aathi11;
-use aathi11;
+create table student;
+use student;
 CREATE TABLE student (studentID INT(5) PRIMARY KEY,studentName VARCHAR(20),DOB INT(8),gender VARCHAR(10),departmentID INT(5) NOT NULL);
 DESC student;
 INSERT INTO student VALUES ("17", "aathiraj", "17042007", "male", "5634");
 SELECT * FROM student;
 DESC student;
-ALTER TABLE student ADD email VARCHAR(50);
+
