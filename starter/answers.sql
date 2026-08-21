98bf62c87e56ef622d403173928450e19e1cf419
1U25CS001-AATHIRAJ <aathirajs29@gmail.com>
2026-08-21 12:35:36 +0530

    Create database and update student table structure
    Created a new database and modified the student table structure, including adding an email column and inserting a record.

--- starter/answers.sql
+++ starter/answers.sql
@@ -1,8 +1,8 @@
-CREATE TABLE Student (
-    StudentID INT PRIMARY KEY,
-    StudentName VARCHAR(20) NOT NULL UNIQUE,
-    DOB DATE NOT NULL,
-    Gender VARCHAR(10) NOT NULL,
-    DepartmentID INT NOT NULL
-);
-
+create database aathi11;
+use aathi11;
+CREATE TABLE student (studentID INT(5) PRIMARY KEY,studentName VARCHAR(20),DOB INT(8),gender VARCHAR(10),departmentID INT(5) NOT NULL);
+DESC student;
+INSERT INTO student VALUES ("17", "aathiraj", "17042007", "male", "5634");
+SELECT * FROM student;
+DESC student;
+ALTER TABLE student ADD email VARCHAR(50);
